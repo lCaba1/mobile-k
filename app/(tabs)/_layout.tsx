b583aa7c039f26dbfc5b7552380aef88ebc501af
Александр <alexander@lcaba1server.local>
2026-03-27 21:23:22 +0300

    tabs icons

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,12 +1,36 @@
 import { Tabs } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function TabsLayout() {
   return (
-    <Tabs>
-      <Tabs.Screen name="convert" options={{ title: "Конвертировать" }} />
-      <Tabs.Screen name="calculate" options={{ title: "Рассчитать" }} />
-      <Tabs.Screen name="history" options={{ title: "История" }} />
+    <Tabs screenOptions={{ tabBarActiveTintColor: "#000000" }}>
+      <Tabs.Screen
+        name="convert"
+        options={{
+          title: "Конвертировать",
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="swap-horizontal" size={24} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="calculate"
+        options={{
+          title: "Рассчитать",
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="calculator" size={24} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="history"
+        options={{
+          title: "История",
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="time-outline" size={24} color={color} />
+          ),
+        }}
+      />
     </Tabs>
   );
 }
-
